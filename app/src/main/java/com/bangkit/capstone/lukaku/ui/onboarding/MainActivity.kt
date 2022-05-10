0bae2895c89d62ed2b0bd0dd3177b6d3fddb9936
Muhammad Rafi Triandi <education.triandy@gmail.com>
2022-05-10 14:37:18 +0700

    adding-library-themes-for-splashAPI-and-marklogo

--- app/src/main/java/com/bangkit/capstone/lukaku/ui/onboarding/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/lukaku/ui/onboarding/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bangkit.capstone.lukaku
+package com.bangkit.capstone.lukaku.ui.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.capstone.lukaku.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
