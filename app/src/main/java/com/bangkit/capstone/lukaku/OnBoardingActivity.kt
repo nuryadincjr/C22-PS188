bd0dfb3e6095ff43d1c3f701d3b418219af8ab00
nuryadincjr <nuryadin.cjr@gmail.com>
2022-05-11 02:41:58 +0700

    add viewBinding, resourcing, and make Onboarding screen with Motion Animation

---
+++ app/src/main/java/com/bangkit/capstone/lukaku/OnBoardingActivity.kt
@@ -0,0 +1,44 @@
+package com.bangkit.capstone.lukaku
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.capstone.lukaku.databinding.ActivityOnBoardingBinding
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        startAction()
+    }
+
+    private fun startAction() {
+        binding.btnJoin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
