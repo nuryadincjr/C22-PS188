324bcb67f879ca0a951aabbdfed400d13bf6f10c
Muhammad Rafi Triandi <education.triandy@gmail.com>
2022-05-11 14:35:50 +0700

    this hello

--- app/src/main/java/com/bangkit/capstone/lukaku/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/lukaku/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Hello1
     }
 }
