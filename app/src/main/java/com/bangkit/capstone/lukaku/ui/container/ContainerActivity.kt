ff24f3576eaad5e772780068d1fa738945ac3551
Muhammad Rafi Triandi <education.triandy@gmail.com>
2022-05-14 02:35:35 +0700

    Fixing animation on SplashScreenAPI

--- app/src/main/java/com/bangkit/capstone/lukaku/ui/container/ContainerActivity.kt
+++ app/src/main/java/com/bangkit/capstone/lukaku/ui/container/ContainerActivity.kt
@@ -1,7 +1,6 @@
 package com.bangkit.capstone.lukaku.ui.container
 
 import android.animation.ObjectAnimator
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,10 +16,10 @@ class ContainerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        installSplashScreenWithAnim().apply {
+            setupPreDrawListener()
+        }
         setContentView(R.layout.activity_container)
-        setupPreDrawListener()
-        exitWithSlideUp()
     }
 
     private fun setupPreDrawListener() {
@@ -42,25 +41,22 @@ class ContainerActivity : AppCompatActivity() {
         )
     }
 
-    private fun exitWithSlideUp() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                // Create custom animation.
-                val slideUp = ObjectAnimator.ofFloat(
-                    splashScreenView,
-                    View.TRANSLATION_Y,
-                    0f,
-                    -splashScreenView.height.toFloat()
-                )
-                slideUp.interpolator = AnticipateInterpolator()
-                slideUp.duration = 200L
-
-                // Call SplashScreenView.remove at the end of the custom animation.
-                slideUp.doOnEnd { splashScreenView.remove() }
-
-                // Run animation.
-                slideUp.start()
-            }
+    private fun installSplashScreenWithAnim() {
+        installSplashScreen().setOnExitAnimationListener { splashScreenView ->
+            // Create custom animation.
+            splashScreenView.iconView.animate().rotation(180F).duration = 500L
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView.iconView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.iconView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 500L
+            // Call SplashScreenView.remove at the end of the custom animation.
+            slideUp.doOnEnd { splashScreenView.remove() }
+            // Run animation.
+            slideUp.start()
         }
     }
 }
